4e957e276f165a1c893324528a0188b2f9071ff3
Peter Kim <peter.kim@uconn.edu>
2022-10-06 00:02:39 -0400

    New timeblocks

--- script.js
+++ script.js
@@ -20,33 +20,51 @@ var updateTime = function () {
 }
 
 // create timeblocks via bootstrap
-function createTimeblocks () {
-    var timeblockContainer = document.getElementById("#timeblock-container");
-    for (i=0; i<timeslots.length; i++)
-    {
-        console.log("This is timeslot: " + timeslots[i]);
-        var timeblockRow = document.createElement("div");
-        timeblockRow.setAttribute("class", "row");
-        document.getElementById("timeblock-container").appendChild(timeblockRow);
+// function createTimeblocks () {
+//     for (i=0; i<timeslots.length; i++)
+//     {
+//         // console.log("This is timeslot: " + timeslots[i]);
+//         var timeblockRow = document.createElement("div");
+//         timeblockRow.setAttribute("class", "row");
+//         document.getElementById("timeblock-container").appendChild(timeblockRow);
 
+//         var timeblockHourElement = document.createElement("div");
+//         timeblockHourElement.setAttribute("class", "col-md-2");
+//         timeblockHourElement.setAttribute("class","columnHour");  
+//         timeblockHourElement.setAttribute("class","time-block");
+//         $(".columnHour").addClass("col-2");      
+//         timeblockHour = document.createElement("h2");
+//         timeblockHour.innerText = timeslots[i];
+//         timeblockHourElement.appendChild(timeblockHour);
+//         timeblockRow.appendChild(timeblockHourElement);
+        
 
-        var timeblockHour = document.createElement("div");
-        timeblockHour.setAttribute("class", "col-2");
-        timeblockRow.appendChild(timeblockHour);
+//         var timeblockMainElement = document.createElement("div");
+//         timeblockMainElement.setAttribute("class", "col-md-8");
+//         timeblockMainElement.setAttribute("class", "columnMain");
+//         timeblockMain = document.createElement("p");
+//         timeblockMainElement.appendChild(timeblockMain);
+//         timeblockRow.appendChild(timeblockMainElement);
 
-        var timeblockMain = document.createElement("div");
-        timeblockMain.setAttribute("class", "col-8");
-        timeblockRow.appendChild(timeblockMain);
+//         var timeblockLockElement = document.createElement("div");
+//         timeblockLockElement.setAttribute("class", "col-md-2");
+//         timeblockLockElement.setAttribute("class","columnLock");
+//         timeblockLock = document.createElement("button");
+//         timeblockLock.setAttribute("type", "button");
+//         timeblockLock.setAttribute("class", "lock-toggle");
+//         timeblockLockElement.appendChild(timeblockLock);  
+//         timeblockRow.appendChild(timeblockLockElement);
+//     }
+//     $(".columnHour").css("padding", "20px");
+//     $(".columnMain").css("background-color","green");
+//     $(".columnLock").css("background-color", "lightblue");
+// }
+// createTimeblocks();
 
-        var timeblockLock = document.createElement("div");
-        timeblockLock.setAttribute("class", "col-2");  
-        timeblockRow.appendChild(timeblockLock);
-    }
-}
-createTimeblocks();
-//TODO: timeblocks for 9am-5pm
 
 //TODO: timeblock color-coding based upon time of day
+//TODO: add color to the Hour / Lock Slots
+//TODO: Add Borders to the timeblocks
 
 //TODO: timeblock event listener (click) ==> enter an event into the timeblock
 
