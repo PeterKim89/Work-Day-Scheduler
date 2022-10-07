40bcbcfd552a68dafea20c4ddab3be16bf270d94
Peter Kim <peter.kim@uconn.edu>
2022-10-06 22:55:52 -0400

    description save feature

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// trigger function upon page load
 // creating the actual timer element at the top of the page
-$(document).ready(function () {
+function createTimer() {
     var timer = document.createElement("h2");
     timer.setAttribute("id", "timer");
     document.getElementById("currentDay").appendChild(timer);
     $("#timer").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));   
-})
+}
 
 // track current hour:minutes
 // call updateTime upon page refresh to update the time.
@@ -13,13 +12,9 @@ var updateTime = function () {
     var currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
     $("#timer").text(currentTime)
 }
-// initial call to updateTime along with a setInterval to keep the timer pseudo-self updating
-updateTime();
-setInterval(updateTime, 1000);
 
 var timeblockSlots = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"]; // timeblock ids
 var timeblockSlot24Hour = [9,10,11,12,13,14,15,16,17]; // timeblock slots converted to their 24 hour values
-
 function colorCode() {
     for (i=0; i<timeblockSlots.length; i++) {
         // if selected timeblock is earlier than current hour, give it the past class and remove present/future classes if they exist
@@ -45,27 +40,35 @@ function colorCode() {
         }
     }
 }
-colorCode();
+
 
 function saveTimeblockDesc () {
-    var saveButtonArray = document.getElementsByClassName(".saveBtn");
-    for (i=0; i<saveButtonArray.length; i++)
+    $(".saveBtn").on("click", function () 
     {
-
-    }
+        var saveText = $(this).siblings(".description").val();
+        var saveTime = $(this).parent().attr("id");
+        // Save text in local storage
+        localStorage.setItem(saveTime, saveText);
+    })
 }
 
-//TODO: timeblock event listener (click) ==> enter an event into the timeblock
+// allows the various timeblock descriptions to stay on the page even after a page refresh
+$("#9am .description").val(localStorage.getItem("9am"));
+$("#10am .description").val(localStorage.getItem("10am"));
+$("#11am .description").val(localStorage.getItem("11am"));
+$("#12pm .description").val(localStorage.getItem("12pm"));
+$("#1pm .description").val(localStorage.getItem("1pm"));
+$("#2pm .description").val(localStorage.getItem("2pm"));
+$("#3pm .description").val(localStorage.getItem("3pm"));
+$("#4pm .description").val(localStorage.getItem("4pm"));
+$("#5pm .description").val(localStorage.getItem("5pm"));
 
-//TODO: save button ==> save event text info for timeblock in local storage
-
-//TODO: localstorage events should persist through page refresh
-
-//TODO: move all function calls into the ready function 
-
-
-    // $(".saveBtn").on("click", function() {
-    //     var text = $(this).siblings(".description").val();
-    //     var time = $(this).parent().attr("id");
-    //     localStorage.setItem(time, text);
-    // })
+// trigger functions upon page load
+$(document).ready(function() {
+    createTimer(); // create the initial jumbotron timer
+    updateTime(); // initial time update
+    setInterval(updateTime, 1000); // keep the timer incrementing and updating itself
+    colorCode(); // color codes the time blocks depending on time of day
+    saveTimeblockDesc(); // allows the timeblock descriptions to be saved to local storage
+}
+)
